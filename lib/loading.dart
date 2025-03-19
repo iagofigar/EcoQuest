eaa243410c9f65ec85257c1e6b636ba13f5d5d8c
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-03-19 15:36:37 +0100

    Loading done

---
+++ lib/loading.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('EcoQuest')),
+        titleTextStyle: const TextStyle(fontSize: 20, color: Colors.white),
+        backgroundColor: const Color(0xFF4CAF50),
+      ),
+      body: Center(
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 200)),
+              Container(
+                width: 270,
+                height: 270,
+                child: Image.asset('assets/EcoQuestLogoNobg.png', fit: BoxFit.fill),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 50)),
+              const Text('   Loading...', style: TextStyle(fontSize: 30, color: Color(0xFF2196f3))),
+            ],
+          )
+      ),
+    );
+  }
+}
