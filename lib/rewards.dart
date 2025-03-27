ed541b75f2dab8a03c0f2e5a665f38b47e6e80aa
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-03-27 18:07:38 +0100

    Reward card creation

--- lib/rewards.dart
+++ lib/rewards.dart
@@ -1,3 +1,4 @@
+import 'package:ecoquest/widgets/reward_card.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -58,85 +59,26 @@ class _RewardsPageState extends State<RewardsPage> {
                 const Padding(padding: EdgeInsets.only(bottom: 50)),
               ],
             ),
-            Wrap(
+            const Wrap(
+              direction: Axis.horizontal,
+              alignment: WrapAlignment.spaceEvenly,
               spacing: 10,
               runSpacing: 10,
               children: [
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    children: [
-                      Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                          ),
-                          width: 75,
-                          height: 100,
-                          child: ClipRRect(
-                              borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                              child: Image.asset('assets/placeholder.jpg', fit: BoxFit.fill))
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 175,
-                              height: 50,
-                              child: const Text('Subpar "copper" ingots', softWrap: true, maxLines: 2, overflow: TextOverflow.ellipsis),
-                            ),
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('1080 '),
-                                Icon(Icons.stars),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                RewardCard(
+                  name: 'Subpar "copper" ingots',
+                  price: 1080,
+                  imageRoute: 'assets/placeholder.jpg',
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    children: [
-                      Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                          ),
-                          width: 75,
-                          height: 100,
-                          child: ClipRRect(
-                              borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                              child: Image.asset('assets/placeholder.jpg', fit: BoxFit.fill))
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 175,
-                              height: 50,
-                              child: const Text('Nanni\'s\n Enemy "Tell Ea-nasir: Nanni sends the following message"', softWrap: true, maxLines: 2, overflow: TextOverflow.ellipsis),
-                            ),
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('1080 '),
-                                Icon(Icons.stars),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                RewardCard(
+                  name: 'Nanni\'s Enemy\n"Tell Ea-nasir: Nanni sends the following message"',
+                  price: 50,
+                  imageRoute: 'assets/placeholder.jpg',
+                ),
+                RewardCard(
+                  name: 'The "Copper" Scroll',
+                  price: 100,
+                  imageRoute: 'assets/placeholder.jpg',
                 ),
               ],
             ),
