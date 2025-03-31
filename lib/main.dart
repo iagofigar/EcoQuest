d8c35ddc0baafdc2d7101ab7c9e2cb971556ec08
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-03-31 16:39:23 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'EcoQuest',
-      initialRoute: '/',
+      initialRoute: '/rewards',
       routes: {
         '/': (context) => const WelcomePage(),
         '/login': (context) => const LoginPage(),
